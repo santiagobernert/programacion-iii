e943595385df4a240dc58d1a70e454b47ca71298
santiagobernert <s.bernertr@alumno.etec.um.edu.ar>
2022-11-07 11:31:25 -0300

    formularios

--- proyecto-fisica-tercero/file.py
+++ proyecto-fisica-tercero/file.py
@@ -95,3 +95,115 @@ for i in range(num_etapas):
 
     print(f"La distancia recorrida es de: {cal_dist(vel_ini, tiempo, acel)}")
     dis_total += cal_dist(vel_ini, tiempo, acel)
+
+
+
+
+
+
+
+
+
+#MISMO CÓDIGO PERO EN FUNCIÓN
+
+
+def calc_vf(acel, tiempo, vel_ini):
+#calculo velocidad final
+        vel_fin = (acel*tiempo)+vel_ini
+        if acel<0: vel_fin=0
+        return vel_fin
+
+def calc_vi(vel_fin, acel, tiempo):
+    #calculo velocidad inicial
+        cal_vi = vel_fin-acel*tiempo 
+        return cal_vi
+
+def calc_tiempo(vel_fin, vel_ini, acel):
+    #calculo tiempo
+        tiempo = (vel_fin-vel_ini)/acel
+        return tiempo
+
+def calc_acel(vel_fin, vel_ini, tiempo):
+    #calculo aceleracion
+            acel = (vel_fin-vel_ini)/tiempo
+            return acel
+
+def calcular(num_etapas, acel_ver, acel, tiempo_ver, tiempo, vi_ver, vi, vf_ver, vf):
+    for i in range(num_etapas):
+
+        print(f"Inicio de la etapa n°{i+1}")
+        
+        n_no = 0
+
+        #aceleracion
+    
+        if acel_ver:
+            tf_acel = True
+            acel = float(input("Introduzca la aceleracion: "))
+        else:
+            tf_acel = False
+            n_no+=1
+            print("")
+
+        #tiempo
+        if n_no == 0:
+            tiempo_ver=input("Sabe el tiempo?(Si/No):\n ")
+            if tiempo_ver.lower()=="si":
+                tf_tiempo = True
+                tiempo = float(input("Introduzca el tiempo: "))
+            else:
+                tf_tiempo = False
+                print("")
+                n_no+=1
+        else: 
+            tf_tiempo = True
+            tiempo = float(input("Introduzca el tiempo: "))
+        
+    
+        if i == 0: 
+            #Velocidad inicial
+            if n_no == 0:
+                vel_ini_ver=input("Sabe la Velocidad Inicial?(Si/No):\n ")
+                if vel_ini_ver.lower()=="si":
+                    vel_ini = float(input("Introduzca la velocidad inicial: "))
+                    tf_vel_in = True
+                else:
+                    tf_vel_in = False
+                    n_no+=1
+                    print("")
+            else: 
+                vel_ini = float(input("Introduzca la velocidad inicial: "))
+                tf_vel_in = True
+        else: 
+            #if tf_vel_fin==True: vel_ini = vel_fin 
+            #elif tf_vel_fin==False: vel_ini = vel_fin
+            vel_ini = vel_fin
+        
+        #Velocidad final
+        if tf_acel==True and tf_tiempo==True and tf_vel_in==True:
+                tf_vel_fin = False
+        else:
+            if n_no==0:
+                vel_fin_ver=input("Sabe la Velocidad Final?(Si/No):\n ")
+                if vel_fin_ver.lower()=="si":
+                    vel_fin = float(input("Introduzca la velocidad final: "))
+                    tf_vel_fin = True
+            else:
+                tf_vel_fin = True 
+                vel_fin = float(input("Introduzca la velocidad final: "))
+                tf_vel_fin = True
+
+        if tf_vel_fin==False:
+            calc_vf(acel, tiempo, vel_ini)
+            
+        if tf_vel_in==False:
+            calc_vi(vel_fin, acel, tiempo)
+
+        if tf_tiempo==False:
+            calc_tiempo(vel_fin, vel_ini, acel)
+
+        if tf_acel==False:
+            calc_acel(vel_fin, vel_ini, tiempo)
+
+        distancia = cal_dist(vel_ini, tiempo, acel)
+        return distancia
--- proyecto-fisica-tercero/script.py
+++ proyecto-fisica-tercero/script.py
@@ -1,23 +1,34 @@
-from xml.dom.minidom import Element
-from file import cal_dist
+from flask import Flask, render_template, request
+from file import cal_dist, cal_vi, calc_acel, calc_tiempo, calc_vf
 
-etapas = Element('etapas').element.value
-tiempo = Element('tiempo').element.value
-aceleracion = Element('aceleracion').element.value
-aceleracion_no = Element('aceleracion-no').element.value
-aceleracion_si = Element('aceleracion-si').element.value
-velocidad_inicial = Element('velocidad_inicial').element.value
-velocidad_inicial_no = Element('velocidad-inicial-no').element.value
-velocidad_inicial_si = Element('velocidad-inicial-si').element.value
-velocidad_final = Element('velocidad_final').element.value
-velocidad_final_no = Element('velocidad-final-no').element.value
-velocidad_final_si = Element('velocidad-final-si').element.value
+app = Flask(__name__)
 
-resp_velocidad_inicial = Element('resp-velocidad-inicial').element.value
-resp_velocidad_final = Element('resp-velocidad-final').element.value
-resp_aceleracion = Element('resp-aceleracion').element.value
-resp_tiempo = Element('resp-tiempo').element.value
-resp_distancia = Element('resp-distancia').element.value
+    
 
-distancia = cal_dist(velocidad_inicial, tiempo, aceleracion)
 
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    datos_a_ingresar = []
+    aceleracion = request.form.get('aceleracion')
+    tiempo = request.form.get('tiempo')
+    velocidad_inicial = request.form.get('vi')
+    velocidad_final = request.form.get('vf')
+    if request.method == 'POST':
+        datos_a_ingresar = {'aceleracion', 'tiempo', 'velocidad_inicial', 'velocidad_final'}.difference([aceleracion, tiempo, velocidad_inicial, velocidad_final])
+        print(datos_a_ingresar)
+    return render_template('home.html', datos_a_ingresar=datos_a_ingresar)
+
+@app.route('/#calcular', methods=['GET'])
+def calcular():
+    aceleracion = request.form.get('aceleracion')
+    tiempo = request.form.get('tiempo')
+    velocidad_inicial = request.form.get('velocidad_inicial')
+    velocidad_final = request.form.get('velocidad_final')
+    if request.method == 'POST':
+        
+    return render_template('home.html', aceleracion=aceleracion, tiempo=tiempo, velocidad_final=velocidad_final, velocidad_inicial=velocidad_inicial, distancia=distancia)
+
+if __name__ == '__main__':
+    app.run(debug=True)
